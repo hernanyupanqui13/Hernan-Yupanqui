47b79ad04726ec4112367be6f1e9beed9c87e09d
Hernan Yupanqui <hernan.yupanqui.prieto@gmail.com>
2021-02-15 08:06:24 -0500

    prueba git remote

--- prueba.py
+++ prueba.py
@@ -1,17 +1,15 @@
 print("Hello friends: !")
-<<<<<<< HEAD
+
 for x in range(5):
   print("I am the best")
-=======
-#for x in range(5):
-  #print("I am the best")
->>>>>>> b319c6a9b869757bd614cf07973ae5d3441f35f1
+
 
 
 print("I can't add numbers :(")
 
 
 print("COntinue working")
+print ("One commit more to check")
 
 
 
